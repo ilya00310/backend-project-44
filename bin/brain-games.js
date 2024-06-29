5aafd5c9185ba06c61056131eeb8dece35a28f02
ilya00310 <ilya@DESKTOP-GGS21KH>
2024-06-29 18:48:48 +0500

    new commit-new live

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,2 @@
 #!/usr/bin/env node
-import { yourName } from "../src/cli.js";
-
-console.log("Welcome to the Brain Games!");
-yourName()
+console.log('Welcome to the Brain Games!');
--- src/cli.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from 'readline-sync';
-
-export const yourName =() => {
-    
-const name= readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-}
-
