d1a00cfb1fe688403bf95f93dd5c92fa92ca5a26
ilya00310 <ilya@DESKTOP-GGS21KH>
2024-07-03 17:30:24 +0500

    last version

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,20 +1,3 @@
-import readlineSync from 'readline-sync';
+import launch from '../src/gameLogic.js';
 
-console.log ('Answer "yes" if the number is even, otherwise answer "no".');
-const random = (min,max) => {
-    min = 1;
-    max = 100;
-    return Math.floor(Math.random()*(100 - 1 + 1)) + min;
-}
-
-for (let i = 0; i < 3; i += 1){
-    const randomNumber = random();
-let question = readlineSync.question(`Question: ${randomNumber}\nYour answer: ` );
-if (question === 'yes' && randomNumber % 2 === 0 || question === 'no' && randomNumber % 2 != 0){
-    console.log('Correct!')
-}else{ 
-(console.log("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!"));
-break;
-}
-console.log('Congratulations, Sam!');
-}
+launch();
--- src/gameLogic.js
+++ src/gameLogic.js
@@ -1,22 +1,19 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-console.log ('Answer "yes" if the number is even, otherwise answer "no".');
-const random = (min,max) => {
-    min = 1;
-    max = 100;
-    return Math.floor(Math.random()*(100 - 1 + 1)) + min;
-}
-
-for (let i = 0; i < 3; i += 1){
-    const randomNumber = random();
-let question = readlineSync.question(`Question: ${randomNumber}\nYour answer: ` );
-if (question === 'yes' && randomNumber % 2 === 0 || question === 'no' && randomNumber % 2 != 0){
-    console.log('Correct!')
-}else{ 
-(console.log("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!"));
-break;
-}
-console.log('Congratulations, Sam!');
-}
-}
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const randomFromOneHundred = () => {
+    return Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  };
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = randomFromOneHundred();
+    const question = readlineSync.question(`Question: ${randomNumber}\nYour answer: `);
+    if ((question === 'yes' && randomNumber % 2 === 0) || (question === 'no' && randomNumber % 2 !== 0)) {
+      console.log('Correct!');
+    } else {
+      (console.log("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!"));
+      break;
+    }
+    console.log('Congratulations, Sam!');
+  }
+};
