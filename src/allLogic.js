306575296b84f4b5de4a9f698ae95b93c2f3dde8
ilya00310 <ilya@DESKTOP-GGS21KH>
2024-07-04 13:00:24 +0500

    architecture try one

---
+++ src/allLogic.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+import yourName from './cli.js';
+
+export default (formulaExpression, correctAnswer, condition) => {
+  const Name = yourName();
+  console.log(condition);
+
+  for (let i = 0; i < 3; i += 1) {
+    const expression = formulaExpression();
+    const checkCorrect = correctAnswer(expression); 
+    const question = readlineSync.question(`Question: ${expression}\nYour answer: `);
+    if (question === 'yes' && checkCorrect === true) {
+      console.log('Correct!');
+    } else if (question === 'no' && checkCorrect === true) {
+      console.log(`${question} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again,${Name}`);
+      return;
+    } else if (question === 'no' && checkCorrect === false) {
+      console.log('Correct!');
+    } else {
+      console.log(`${question} is wrong answer ;(. Correct answer was 'no'.\nLet's try again,${Name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations,${Name}`);
+};
--- src/gameLogic.js
+++ src/gameLogic.js
@@ -1,26 +1,5 @@
-import readlineSync from 'readline-sync';
-import yourName from './cli.js';
+import allLogic from './allLogic.js';
 
-export default () => {
-  const Name = yourName();
-  console.log('\nAnswer "yes" if the number is even, otherwise answer "no".');
-
-  const randomFromOneHundred = () => Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-
-  for (let i = 0; i < 3; i += 1) {
-    const randomNumber = randomFromOneHundred();
-    const question = readlineSync.question(`Question: ${randomNumber}\nYour answer: `);
-    if (question === 'yes' && randomNumber % 2 === 0) {
-      console.log('Correct!');
-    } else if (question === 'no' && randomNumber % 2 === 0) {
-      console.log(`${question} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again,${Name}`);
-      return;
-    } else if (question === 'no' && randomNumber % 2 !== 0) {
-      console.log('Correct!');
-    } else {
-      console.log(`${question} is wrong answer ;(. Correct answer was 'no'.\nLet's try again,${Name}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations,${Name}`);
-};
+const randomFromOneHundred = () => Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+const checkCondition = (x) => x % 2 === 0; 
+allLogic(randomFromOneHundred,checkCondition, '\nAnswer "yes" if the number is even, otherwise answer "no".');
