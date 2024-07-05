8d42984ff4677ca0a84b9d3eeefb15c816083399
ilya00310 <ilya@DESKTOP-GGS21KH>
2024-07-05 21:44:34 +0500

    last fix ?

--- src/b-even modul.js
+++ src/b-even modul.js
@@ -2,10 +2,10 @@ export default () => Math.floor(Math.random() * (100 - 1 + 1)) + 1;
 export const myConditionIf = (userResponse, name, number) => {
   if (userResponse === 'yes' && number % 2 === 0) {
     return console.log('Correct!');
-  } if (userResponse === 'no' && number % 2 === 0) {
-    return console.log(`${userResponse} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again,${name}`);
-  } if (userResponse === 'no' && number % 2 !== 0) {
+  }
+  if (userResponse === 'no' && number % 2 !== 0) {
     return console.log('Correct!');
   }
-  return console.log(`${userResponse} is wrong answer ;(. Correct answer was 'no'.\nLet's try again,${name}!`);
+  console.log(`${userResponse} is wrong answer ;(. Correct answer was 'no'.\nLet's try again,${name}!`);
+  return false;
 };
