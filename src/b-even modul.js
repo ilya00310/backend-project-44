01c990a8a53d4d3dd5d5f67d692e06965d322c3c
ilya00310 <ilya@DESKTOP-GGS21KH>
2024-07-10 17:12:43 +0500

    fix abstraction lvl

--- src/b-even modul.js
+++ src/b-even modul.js
@@ -2,12 +2,12 @@ import getrandomNumberInRange from './utils.js';
 
 export default () => getrandomNumberInRange(1, 100);
 export const сheckCondition = (userResponse, name, number) => {
-  if (userResponse === 'yes' && number % 2 === 0) {
+  const isEven = number % 2 === 0;
+  const expectedAnswer = isEven ? 'yes' : 'no';
+  if (userResponse === expectedAnswer) {
     return console.log('Correct!');
   }
-  if (userResponse === 'no' && number % 2 !== 0) {
-    return console.log('Correct!');
-  }
+
   console.log(`${userResponse} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
   return false;
 };
