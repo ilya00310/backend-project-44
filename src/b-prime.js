4d25e448ea3f6a7c9e751c384ea8e1e98ed90fd9
ilya00310 <ilya@DESKTOP-GGS21KH>
2024-07-12 21:48:55 +0500

    b-prime is work

--- src/b-prime.js
+++ src/b-prime.js
@@ -1,20 +1,19 @@
 import getrandomNumberInRange from './utils.js';
 
 const isPrime = (num) => {
-  if (+num === 1) {
+  if (num === 1) {
     return false;
   }
   for (let i = 2; i < Math.sqrt(num); i += 1) {
     if (num % i === 0) {
-      return true;
+      return false;
     }
   }
-  return false;
+  return true;
 };
 
 export default () => {
-  const questionAndAnswer = [];
   const question = (getrandomNumberInRange(1, 100));
-  const answer = (isPrime(`${questionAndAnswer[0]}`)) ? 'yes' : 'no';
+  const answer = (isPrime(`${question}`)) ? 'yes' : 'no';
   return [question, answer];
 };
