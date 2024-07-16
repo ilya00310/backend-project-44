b153a29f4cc84c4361d7aa0c8aa5b645836a1b05
ilya <kotena1980@bk.ru>
2024-07-16 21:33:35 +0500

    fix export

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,2 @@
-export default (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+/* eslint-disable import/prefer-default-export */
+export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
